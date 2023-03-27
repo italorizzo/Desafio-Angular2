0e077a948607dbbcc341bf0f18d09940f2742507
italorizzo <italorizzo@outlook.com>
2023-03-27 10:38:59 -0300

    atualizacao

--- e2/src/app/app-routing.module.ts
+++ e2/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'botao1', loadChildren: () => import('./botao1/botao1.module').then((module)=>module.Botao1Module)},
   {path: 'botao2/:id', component: Botao2Component},
-  {path: 'botao3/:id', loadChildren: ()=>import('./botao3/botao3.module').then((module)=>module.Botao3Module)}
+  {path: 'botao3/:id', loadChildren: ()=> import('./botao3/botao3.module').then((module)=>module.Botao3Module)}
 ];
 
 export const routing: ModuleWithProviders<any> = RouterModule.forRoot(routes)
